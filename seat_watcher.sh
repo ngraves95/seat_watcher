6b65aa14ab97d7009c1da79c14492e61a5101a81
ngraves95 <ngraves95@gmail.com>
2016-04-02 13:02:14 -0400

    Stop email spamming

--- seat_watcher.sh
+++ seat_watcher.sh
@@ -18,14 +18,41 @@ crn_to_url() {
     echo $base_url$1
 }
 
-parse_data() {
-    while read data;
-    do
-    	echo $data
-    done
+pack_seats() {
+    echo "$1;$2"
 }
 
-pipe_to_array() {
+# Usage:
+#    compare_seats "packed;seats" remaining waitlist
+check_seats_differ() {
+
+    if [[ $# -lt 3 ]]
+    then
+	echo "First time" >&2
+	echo true
+	return
+    fi
+
+    old_seats=(${1//;/ })
+
+    if [[ ${old_seats[0]} != $2 ]]
+    then
+	echo true
+	return
+    fi
+
+    if [[ ${old_seats[1]} != $3 ]]
+    then
+	echo true
+	return
+    fi
+
+    echo false
+}
+
+declare -A persist
+
+notify_user() {
     declare -a parsed_data
     i=0
     while read data;
@@ -40,16 +67,28 @@ pipe_to_array() {
 
     if [[ $remaining_seats != "0" || $waitlist_remaining != "0" ]]
     then
-	echo $name: Remaining: $remaining_seats Waitlist Remaining: $waitlist_remaining | mail -s "Remaining Seats Update" $email_address
+	if [[ `check_seats_differ ${persist["$name"]} $remaining_seats $waitlist_remaining` == true ]]
+	then
+	    echo "Sending mail" >&2
+	    echo $name: Remaining: $remaining_seats Waitlist Remaining: $waitlist_remaining | mail -s "Remaining Seats Update" $email_address
+	else
+	    echo "No difference found" >&2
+	fi
     fi
+
+    echo "$name;$remaining_seats;$waitlist_remaining"
 }
+
 
 
 while true
 do
     for crn in $@
     do
-	crn_to_url $crn | xargs curl 2>/dev/null | grep -E "<t.*CLASS=\"dd"| sed 's/<[^>]*>//g' | pipe_to_array
+	updates=`crn_to_url $crn | xargs curl 2>/dev/null | grep -E "<t.*CLASS=\"dd"| sed 's/<[^>]*>//g' | notify_user`
+	name=`expr match "$updates" '\([^;]*\)'`
+	seats=`expr match "$updates" '[^;]*;\(.*\)'`
+	persist["$name"]=$seats
     done
-    sleep 30
+    sleep 5
 done
